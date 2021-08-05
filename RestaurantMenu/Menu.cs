00e0434bd4c2d78506bab04b70c43204c1c545c9
Tiah Keefe <tetered12@gmail.com>
2021-08-04 20:10:39 -0500

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public List<MenuItem>Items { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public Menu(List<MenuItem> items, DateTime lastUpdated)
+        {
+            Items = items;
+            LastUpdated = lastUpdated;
+
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Catagory { get; set; }
+        public bool IsNew { get; set}
+
+        public MenuItem(double price, string description, string catagory, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Catagory = catagory;
+            IsNew = isNew;
+        }
+    }
+}
