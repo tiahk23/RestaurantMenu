b25cecc5aef5c8386ef781575ee1e8b825af3e7f
Tiah Keefe <tetered12@gmail.com>
2021-08-11 12:40:47 -0500

    Made changes to the meny and program classes

--- RestaurantMenu/Menu.cs
+++ RestaurantMenu/Menu.cs
@@ -8,15 +8,19 @@ namespace RestaurantMenu
 {
     public class Menu
     {
+        
+
         public List<MenuItem>Items { get; set; }
 
         public DateTime LastUpdated { get; set; }
+        public IEnumerable<object> MenuItem { get; internal set; }
 
-        public Menu(List<MenuItem> items, DateTime lastUpdated)
+        public Menu(List<MenuItem> menuItem)
         {
-            Items = items;
-            LastUpdated = lastUpdated;
+            this.MenuItem = menuItem;
+            LastUpdated = DateTime.Now;
 
         }
+
     }
 }
--- RestaurantMenu/Program.cs
+++ RestaurantMenu/Program.cs
@@ -9,8 +9,14 @@ namespace RestaurantMenu
             MenuItem Beans = new MenuItem(2.5, "Yummy black beans!", "Sides", true);
             MenuItem GreenChilli = new MenuItem(2.5, "Colorado Green Chilli!", "Sides", true);
             MenuItem BeefBurrito = new MenuItem(3.0, "Fried beef wrapped in a flower tortilla!", "Main Course", true);
-            Console.WriteLine(MenuItem.Equals());
+            Menu mexicanMenu = new Menu(new System.Collections.Generic.List<MenuItem> { Beans, GreenChilli, BeefBurrito});
 
+            foreach(var item in mexicanMenu.MenuItem)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
+
+        
     }
 }
